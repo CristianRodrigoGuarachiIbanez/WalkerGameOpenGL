3bcfb229f3a5f85c1b3321c9098ff709465bba92
Cristian Rodrigo Guarachi Ibanez <cristguarachi@gmail.com>
2022-02-09 12:42:17 +0100

    assimp

---
+++ modelExporter.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <string>
+#include <fstream>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+//https://www.youtube.com/watch?v=hYZNN0MTLuc&t=538s
+// https://github.com/assimp/assimp
+
+struct Position{
+    float x,y,z;
+};
+
+class ModelExporter{
+    private:
+        std::vector<Position> positions;
+        std::vector<uint32_t> indices;
+    public:
+        std::vector<Position> get_positions(){
+            return positions;
+        }
+        std::vector<uint32_t> get_indices(){
+            return indices;
+        }
+        void processMesh(aiMesh* mesh, const aiScene* scene);
+        void processNode(aiNode* node, const aiScene* scene);
+        char* getFilename(char* filename);
+};
